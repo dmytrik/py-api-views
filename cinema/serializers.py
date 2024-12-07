2820a0b3c79a90eb68cb3920e184ec98cfdd2003
dmytro <dmysamo@gmail.com>
2024-12-07 14:41:39 +0200

    add read_only_fields

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -12,21 +12,25 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ("id", "title", "description", "duration")
+        read_only_fields = ("id",)
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ("id", "name")
+        read_only_fields = ("id",)
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
         fields = ("id", "name", "rows", "seats_in_row")
+        read_only_fields = ("id",)
 
 
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name")
+        read_only_fields = ("id",)
